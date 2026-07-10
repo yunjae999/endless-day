2be4a624eaee77c8301f71b8fb78ff80482ecc8d
you774926-spec <gfbyj99@gmail.com>
2026-07-10 11:37:06 +0900

    [FEAT] 플레이어 이동 및 달리기 구현 #8
    WASD 기반 플레이어 이동과 Shift 입력을 이용한 달리기 기능을 구현했다.
    
    - WASD 입력 기반 이동 처리
    - Shift 입력 시 달리기 상태 적용
    - 이동 / 달리기 상태에 따른 속도 변경
    - Idle / Move / Run 애니메이션 연결
    - 이동 입력 여부에 따른 애니메이션 전환 처리
    
    Refs #8

--- EndlessDay/Assets/2. Scripts/Objects/PlayerController.cs
+++ EndlessDay/Assets/2. Scripts/Objects/PlayerController.cs
@@ -1,19 +1,117 @@
-using UnityEngine;
 using Defines;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     Animator _animator;
     PlayerActionState _currentState;
 
+    Vector3 _moveDir;
+
+    bool _runInput;
+    bool _isRun;
+
     void Awake()
     {
-        _animator = GetComponentInChildren<Animator>();  
+        _animator = GetComponentInChildren<Animator>();
+        _currentState = PlayerActionState.IDLE;
+    }
+    void Update()
+    {
+        PlayerProcess();
     }
 
+    void PlayerProcess()
+    {
+        switch (_currentState)
+        {
+            case PlayerActionState.IDLE:
+                // Move로 전환
+                if(HasMoveInput())
+                    ChangeActionState(PlayerActionState.MOVE);
+                break;
+            case PlayerActionState.MOVE:
+                // Idle로 전환
+                if (!HasMoveInput())
+                {
+                    SetRun(false);
+                    ChangeActionState(PlayerActionState.IDLE);
+                    return;
+                }
+
+                UpdateRun();
+                // 이동
+                Move();
+                //회전
+                Rotate();
+                break;
+            case PlayerActionState.ATTACK:
+                break;
+            case PlayerActionState.SKILL:
+                break;
+        }
+    }
     public void ChangeActionState(PlayerActionState state)
     {
-        _animator.SetInteger("ActionState", (int)state);
+        if (_currentState == state)
+            return;
+
         _currentState = state;
+        _animator.SetInteger("ActionState", (int)_currentState);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveDir = value.Get<Vector2>();
+        _moveDir = new Vector3(
+            moveDir.x,
+            0f,
+            moveDir.y
+        );
+    }
+    public void OnRun(InputValue value)
+    {
+        _runInput = value.isPressed;
+        Debug.Log(_runInput);
+    }
+    void SetRun(bool isRun)
+    {
+        if (_isRun == isRun)
+            return;
+
+        _isRun = isRun;
+    }
+    void UpdateRun()
+    {
+        bool shouldRun =
+            _runInput &&
+            _currentState == PlayerActionState.MOVE &&
+            HasMoveInput();
+
+        SetRun(shouldRun);
+    }
+    void Move()
+    {        
+        transform.position += _moveDir.normalized * 2f * Time.deltaTime;
+    }
+    void Rotate()
+    {
+        if(_moveDir.sqrMagnitude < 0.01f)
+            return;
+
+        Quaternion targetRotation =
+            Quaternion.LookRotation(_moveDir);
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            2f * Time.deltaTime
+        );
+    }
+    bool HasMoveInput()
+    {
+        return _moveDir.sqrMagnitude > 0.01f;
     }
 }
